78b70e9cd8886af201e6f8e8f1ab243134db635d
taow2 <taow2@users.noreply.github.com>
2023-04-24 11:13:02 +0800

    Add files via upload

--- js/news.js
+++ js/news.js
@@ -3,6 +3,6 @@ let news_all = document.getElementById('news_list');
 let news = document.createElement('li');
 news.classList.add('list-group-item');
 
-news.innerHTML ='<time class="pe-5">2023/04/24</time><a href="news_23_04_24.html"><span class="nowrap">XPONENTIAL 2023</span></a>';
+news.innerHTML ='<time class="pe-5">2023/04/24</time><a href="news_23_04_24.html"><span class="nowrap">XPONENTIAL</span></a>';
 news_all.appendChild(news);
 
