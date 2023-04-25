5cba26f4da226b12bd9a1b437965a2ca4cc4d1b7
taow2 <taow2@users.noreply.github.com>
2023-04-25 15:02:51 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 // navbar adjust
-let nav_bar = document.getElementById('nav_id');
 /*
+let nav_bar = document.getElementById('nav_id');
+
 nav_bar.classList.add('');
 nav_bar.classList.remove('');
+*/
+
+let nav_brand = document.getElementById('brand');
+
+nav_brand.classList.add('fw-bold');
+
+
+// footer
+/*
+let footer = document.getElementById('foot');
 */
 
