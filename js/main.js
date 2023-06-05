a167785fa9bc8433108e40baa1b68e2015138c07
taow2 <taow2@users.noreply.github.com>
2023-06-05 14:52:17 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,53 @@
+//for preventing JS flickering
+let body = document.getElementById('body-id');
+body.classList.remove('d-none');
+
+// Switch Language
+
+let text_eng = document.querySelectorAll('.eng');
+let text_cn = document.querySelectorAll('.cn');
+
+console.log(text_eng.length);
+console.log(text_cn.length);
+
+if(localStorage.getItem("language") == null || localStorage.getItem("language") == "eng") {
+    for (i = 0; i < text_eng.length; i++) {
+        if(!text_cn[i].classList.contains('d-none')) {
+            text_cn[i].classList.add('d-none');
+        }
+        if(text_eng[i].classList.contains('d-none')) {
+            text_eng[i].classList.remove('d-none');
+        }
+    }
+}
+else {
+    for (i = 0; i < text_eng.length; i++) {
+        if(!text_eng[i].classList.contains('d-none')) {
+            text_eng[i].classList.add('d-none');
+        }
+        if(text_cn[i].classList.contains('d-none')) {
+            text_cn[i].classList.remove('d-none');
+        }
+    }
+}
+
+function switch_lang() {
+    if(localStorage.getItem("language") == null || localStorage.getItem("language") == "eng") {
+        localStorage.setItem("language", "cn");
+        for (i = 0; i < text_eng.length; i++) {
+            text_eng[i].classList.add('d-none');
+            text_cn[i].classList.remove('d-none');
+        }
+    }
+    else {
+        localStorage.setItem("language", "eng");
+        for (i = 0; i < text_eng.length; i++) {
+            text_cn[i].classList.add('d-none');
+            text_eng[i].classList.remove('d-none');
+        }
+    }
+}
+
 // navbar adjust
 /*
 let nav_bar = document.getElementById('nav_id');
@@ -11,8 +61,4 @@ let nav_brand = document.getElementById('brand');
 nav_brand.classList.add('fw-bold');
 
 
-// footer
-/*
-let footer = document.getElementById('foot');
-*/
 
